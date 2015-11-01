fd8d2d926f77693e8cd71e9b100ea85971737315
Amitai Burstein <amitai@gizra.com>
2015-11-01 20:49:01 +0200

    Improve test

--- qa.js
+++ qa.js
@@ -1,12 +1,18 @@
 runTests = function() {
   return [
     {
-      id: "it should find the add to cart button",
-      result: true
+      id: "it should find the a visible add to cart link",
+      result: function() {
+        var element = document.querySelector('a.cart');
+        return !!element && element.offsetParent !== null;
+      }
     },
     {
-      id: "it should find the title",
-      result: false
+      id: "it should find a visible title",
+      result: function() {
+        var element = document.querySelector('h1');
+        return !!element && element.offsetParent !== null;
+      }
     }
   ]
 }
@@ -14,7 +20,7 @@ runTests = function() {
 main = function() {
   result = runTests();
   result.forEach(function(row) {
-    if (!!row.result) {
+    if (!!row.result()) {
       return;
     }
 
